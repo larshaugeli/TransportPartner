c863f833e76fd943d9f27923371254a28a696f0f
Lars Henrik Mostad Haugeli <LarsHenrik.MostadHaugeli@infotjenester.no>
2020-03-20 11:48:07 +0100

    Created controller, viewmodels and views

---
+++ TransportPartner/Controllers/DeliveryController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TransportPartner.Controllers
+{
+    public class DeliveryController : Controller
+    {
+        // GET: Delivery
+        public ActionResult Overview()
+        {
+            return View();
+        }
+
+        // GET: Delivery/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Delivery/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Delivery/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Overview));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Delivery/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Delivery/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Overview));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Delivery/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Delivery/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Overview));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ TransportPartner/Models/CarViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace TransportPartner.Models
+{
+    public class CarViewModel
+    {
+        
+    }
+}
